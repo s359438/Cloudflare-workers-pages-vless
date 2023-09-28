2928f651f32eb8a166cac08ff34b27806b51d0c4
s359438 <143267146+s359438@users.noreply.github.com>
2023-09-29 02:41:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b7e78d85-f16c-42d6-887d-94e0ed0fe80d';
 
-const proxyIPs = ['104.18.174.139']; // ['download.yunzhongzhuan.com', '104.18.95.215', '104.18.128.6', '104.17.131.167', 'www.glassdoor.com'];
+const proxyIPs = ['192.203.230.109']; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
